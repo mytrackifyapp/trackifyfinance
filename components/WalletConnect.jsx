f649551a433bb2c3b85b8fe62f326a6ba15cc691
rizzbrand <rizzbrandit@gmail.com>
2025-09-19 16:16:19 +0000

    newchanges

--- components/WalletConnect.jsx
+++
@@ -1,8 +0,0 @@
-// components/WalletConnect.jsx
-"use client";
-import React from "react";
-import { ConnectButton } from "thirdweb/react";
-
-export default function WalletConnect() {
-  return <ConnectButton />;
-}
