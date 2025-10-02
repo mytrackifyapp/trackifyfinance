e1a041cbfdbc9b98d4fb4987a4ba61564c9c7291
rizzbrand <rizzbrandit@gmail.com>
2025-10-02 18:53:08 +0000

    update2

--- app/(main)/pricing/page.jsx
+++
@@ -1,9 +0,0 @@
-import { PricingTable } from '@clerk/nextjs'
-
-export default function PricingPage() {
-  return (
-    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '0 1rem' }}>
-      <PricingTable />
-    </div>
-  )
-}
