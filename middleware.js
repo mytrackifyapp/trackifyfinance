19247d4e380c721fdc1b28e4cf7e043370575036
rizzbrand <rizzbrandit@gmail.com>
2025-10-04 20:42:28 +0000

    update17.8

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -7,16 +7,19 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect(); // block if not signed in
+export default clerkMiddleware(async (auth, req) => {
+  const { isSignedIn } = auth();
+
+  if (isProtectedRoute(req) && !isSignedIn) {
+    return redirectToSignIn({ returnBackUrl: req.url });
   }
+
   return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|api/inngest|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };
