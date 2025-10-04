1ee80dc80094bbbdf455bb57f1b22bb3a51f08dc
rizzbrand <rizzbrandit@gmail.com>
2025-10-04 20:36:34 +0000

    update17.7

--- middleware.js
+++ middleware.js
@@ -16,7 +16,7 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|api/inngest|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };
